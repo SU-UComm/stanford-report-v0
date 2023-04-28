ca0e74db007c578ee4b6b88af0c935628d0dcccb
Marcin Rek <mrek@squiz.pl>
2023-04-28 19:32:35 +0200

    fixes

--- cutup/stanford-news/src/components/su-react_component/src/react_component/client.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {hydrate} from 'react-dom';
-
-import SampleComponent from '../jsx/SampleComponent.jsx';
-
-document.querySelectorAll('[data-hydration-component="su-react_component"]').forEach((element) => {
-    const props = JSON.parse(element.dataset.hydrationProps);
-    hydrate(<SampleComponent {...props} />, element);
-});
