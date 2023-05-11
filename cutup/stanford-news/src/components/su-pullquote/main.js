6f36b9779fade66e489be3e8f6d1b9323dc1b482
Marcin Rek <mrek@squiz.pl>
2023-05-11 15:08:02 +0200

    rebuild dxpLoader, add new webpack config, component fixes

--- cutup/stanford-news/src/components/su-pullquote/main.js
+++ cutup/stanford-news/src/components/su-pullquote/main.js
@@ -1,4 +1,35 @@
+function renderImage(image, alt) {
+    if (image) {
+        let srcset = [];
+        image.imageVariations.aspectRatios.forEach((img) => {
+            if (img.width === 100 && img.height === 100) {
+                srcset.push(`${img.url} 100w`);
+            } else if (img.width === 200 && img.height === 200) {
+                srcset.push(`${img.url} 200w`);
+            } else if (img.width === 555 && img.height === 555) {
+                srcset.push(`${img.url} 555w`);
+            } else if (img.width === 795 && img.height === 795) {
+                srcset.push(`${img.url} 795w`);
+            } else if (img.width === 960 && img.height === 960) {
+                srcset.push(`${img.url} 960w`);
+            } else if (img.width === 1110 && img.height === 1110) {
+                srcset.push(`${img.url} 1110w`);
+            } else if (img.width === 1343 && img.height === 1343) {
+                srcset.push(`${img.url} 1343w`);
+            }
+        });
+
+        return `<img src="${image.imageVariations.original.url}" class="headshot" alt="${alt}" decoding="async" ${
+            srcset ? `srcset="${srcset.join(', ')}"` : ''
+        } sizes="(max-width: 100px) 100vw, 100px" loading="eager"/>`;
+    } else {
+        return '';
+    }
+}
+
 module.exports = async (input) => {
+    const image = renderImage(input.image, input.attribution);
+
     let alignmentClass;
     switch (input.alignment) {
         case 'left':
@@ -16,16 +47,11 @@ module.exports = async (input) => {
 
     return `
     <div class="pullquote ${alignmentClass}">
-        <img src="${input?.image?.imageVariations?.original?.url}" class="headshot" alt="${
-        input.attribution
-    }" decoding="async" srcset="https://news.stanford.edu/wp-content/uploads/2018/10/Kovscek_square-100x100.jpg 100w, https://news.stanford.edu/wp-content/uploads/2018/10/Kovscek_square-555x555.jpg 555w, https://news.stanford.edu/wp-content/uploads/2018/10/Kovscek_square-795x795.jpg 795w, https://news.stanford.edu/wp-content/uploads/2018/10/Kovscek_square-960x960.jpg 960w, https://news.stanford.edu/wp-content/uploads/2018/10/Kovscek_square.jpg 1343w, https://news.stanford.edu/wp-content/uploads/2018/10/Kovscek_square-100x100@2x.jpg 200w, https://news.stanford.edu/wp-content/uploads/2018/10/Kovscek_square-555x555@2x.jpg 1110w" sizes="(max-width: 100px) 100vw, 100px" loading="eager" />
+        ${image}
         <blockquote>
-            <p>“${input.quote}’”</p>
+            <p>“${input.quote}”</p>
             <p class="quote-attrib">—${input.attribution}</p>
             <p class="quote-attrib supplemental">${input.description}</p>
         </blockquote>
-    </div>
-    <pre><code>
-        ${JSON.stringify(input, null, 2)}
-    </code></pre>`;
+    </div>>`;
 };
