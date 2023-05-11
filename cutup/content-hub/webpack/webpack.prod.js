0403b686320d863d1c7a3fcaced0173ec48a795f
Marcin Rek <mrek@squiz.pl>
2023-05-11 07:23:26 +0200

    pullquote wip, error on frontend regarding SquizImage

--- cutup/content-hub/webpack/webpack.prod.js
+++ cutup/content-hub/webpack/webpack.prod.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const {merge} = require('webpack-merge');
 const common = require('./webpack.common.js');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
@@ -7,7 +6,22 @@ const Dotenv = require('dotenv-webpack');
 const config = require('./config');
 
 module.exports = (env) => {
+    // Handle entries list
+    let buildEntries = Object.assign({}, config.entry);
+    delete common.entry;
+
+    // Keep only server builds if required
+    if (env.serverOnly) {
+        Object.keys(buildEntries).forEach((entry) => {
+            if (buildEntries[entry]?.library?.type !== 'commonjs2') {
+                delete buildEntries[entry];
+            }
+        });
+        console.log(buildEntries);
+    }
+
     return merge(common, {
+        entry: buildEntries,
         mode: 'production',
         devtool: 'source-map',
         module: {
@@ -31,19 +45,25 @@ module.exports = (env) => {
                 },
             ],
         },
-        plugins: [
-            new Dotenv({
-                path: `.env`,
-            }),
-            new MiniCssExtractPlugin({
-                filename: 'css/[name].css',
-                chunkFilename: 'css/[name].css',
-            }),
-            new CleanWebpackPlugin({
-                cleanAfterEveryBuildPatterns: [config.buildFolder],
-                verbose: true,
-            }),
-        ],
+        plugins: env.serverOnly
+            ? [
+                  new Dotenv({
+                      path: `.env`,
+                  }),
+              ]
+            : [
+                  new Dotenv({
+                      path: `.env`,
+                  }),
+                  new MiniCssExtractPlugin({
+                      filename: 'css/[name].css',
+                      chunkFilename: 'css/[name].css',
+                  }),
+                  new CleanWebpackPlugin({
+                      cleanAfterEveryBuildPatterns: [config.buildFolder],
+                      verbose: true,
+                  }),
+              ],
         optimization: {
             minimize: env.minify === 'true' ? true : false,
         },
