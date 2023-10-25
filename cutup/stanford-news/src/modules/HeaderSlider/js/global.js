db87018fc8818c0c7213ce19cb8f95a86b6c67bc
Daniel <dkaczmarek@squiz.pl>
2023-10-25 14:05:29 +0200

    fix: moved header slider into separate module

---
+++ cutup/stanford-news/src/modules/HeaderSlider/js/global.js
@@ -0,0 +1,14 @@
+const headerSlider = document.querySelector('.basic-story__header-slider');
+const swiper = new Swiper(headerSlider, {
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.basic-story__header-slider-next',
+        prevEl: '.basic-story__header-slider-prev',
+    },
+
+    slidesPerView: 'auto',
+    spaceBetween: 26,
+    a11y: true,
+});
